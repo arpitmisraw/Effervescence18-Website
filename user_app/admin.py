e3f31070f33ce4b27b4a1ef7736cabe498cfe02f
aswinzz <iit2016106@iiita.ac.in>
2018-08-14 20:38:56 +0530

    Add user csv

--- user_app/admin.py
+++ user_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import RegularUser, Event, File
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 def export_csv(modeladmin, request, queryset):
     import csv
     from django.utils.encoding import smart_str
@@ -32,10 +35,32 @@ def export_csv(modeladmin, request, queryset):
     return response
 export_csv.short_description = u"Export CSV"
 
-class MyModelAdmin(admin.ModelAdmin):
-    actions = [export_csv]
+def export_csv_user(modeladmin, request, queryset):
+    import csv
+    from django.utils.encoding import smart_str
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=mymodel.csv'
+    writer = csv.writer(response, csv.excel)
+    response.write(u'\ufeff'.encode('utf8')) # BOM (optional...Excel needs it to open UTF-8 file properly)
+    writer.writerow([
+        smart_str(u"ID"),
+        smart_str(u"Username"),
+        smart_str(u"Email"),
+    ])
+    for obj in queryset:
+        writer.writerow([
+            smart_str(obj.pk),
+            smart_str(obj.username),
+            smart_str(obj.email),
+        ])
+    return response
+export_csv.short_description = u"Export CSV"
+
+class UserAdmin(admin.ModelAdmin):
+    actions = [export_csv_user]
 
 admin.site.register(RegularUser,MyModelAdmin)
+admin.site.register(User,UserAdmin)
 admin.site.register(Event)
 admin.site.register(File)
 
